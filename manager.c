cf994a34c2f5aa7158a606e42c0bfbb24ff21279
GaEunJang <22100628@handong.ac.kr>
2022-04-25 17:28:57 +0900

    loadData

--- manager.c
+++ manager.c
@@ -56,7 +56,15 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
+	fp=fopen("product.txt","rt");
+	if(fp==NULL){
+		printf("=>파일이 없음\n");
+	}
+	for(;;count++){
+		fscanf(fp,"%d, %d, %s\n",&p[count].weight,&p[count].price,p[count].name);
+	if(feof(fp)) break;
+	}
+	fclose(fp);
 
 
 
